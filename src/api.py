41b651b3dc3fc8e6bad01bbc1c3863727aa5bc51
Vincent <47754918+Chimp56@users.noreply.github.com>
2025-02-28 12:16:21 -0600

    Updated API doc name

--- src/api.py
+++ src/api.py
@@ -1,5 +1,5 @@
 from ninja import NinjaAPI
 from src.controller.order_controller import router as order_router
 
-api = NinjaAPI()
+api = NinjaAPI(title="InstaTry API", docs_url="/docs")
 api.add_router("/orders", order_router)
